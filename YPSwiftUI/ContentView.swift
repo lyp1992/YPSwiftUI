02287dadb74352ec972532fe174484e67230e693
lyp1992 <2294582526@qq.com>
2019-09-22 22:12:54 +0800

    Initial Commit

---
+++ YPSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YPSwiftUI
+//
+//  Created by 赖永鹏 on 2019/9/22.
+//  Copyright © 2019 LYP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
